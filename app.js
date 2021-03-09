b239203202ffbb5cefa905ec29b349a7bdf7267a
Papeya-taahaa <papeyataha@gmai.com>
2021-03-09 22:37:52 +0600

    javascript commit

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log(5);
+console.log(2+3);
+
+var rickshawVara = 50;
+console.log(rickshawVara);
+
+// declare age
+
+var age = 23;
+console.log(age);
+ 
+
+// string
+
+var name = "kolim";
+console.log(name);
